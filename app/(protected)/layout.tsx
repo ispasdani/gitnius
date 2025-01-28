4f3db9be0cafed20562dbf2dd72538c288632066
ispasdani <ispasdani14@gmail.com>
2025-01-28 22:42:40 +0100

    solved a hight issue

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -19,7 +19,7 @@ const SidebarLayout = ({ children }: SidebarLayoutProps) => {
         </div>
         <div className="h-4"></div>
         {/* main content */}
-        <div className="border-sidebar-border bg-sidebar border shadow roudned-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
+        <div className="border-sidebar-border bg-sidebar border shadow roudned-md overflow-y-scroll h-[calc(100vh-5rem)] p-4">
           {children}
         </div>
       </main>
