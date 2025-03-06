49f368ea3e129a629efbf031f0ff0d40943791e0
ispasdani <ispasdani14@gmail.com>
2025-03-06 23:05:10 +0100

    sidebar work 46:06

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,3 +1,4 @@
+import AppSidebar from "@/components/AppSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
@@ -9,16 +10,16 @@ interface SidebarLayoutProps {
 const SidebarLayout = ({ children }: SidebarLayoutProps) => {
   return (
     <SidebarProvider>
-      {/* <AppSidebar /> */}
+      <AppSidebar />
       <main className="w-full m-2">
-        <div className="flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
+        <div className="flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2">
           {/* <SearchBar /> */}
           <div className="ml-auto"></div>
           <UserButton />
         </div>
         <div className="h-4"></div>
         {/* Main Content*/}
-        <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-rem)] p-4">
+        <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-5rem)] p-4">
           {children}
         </div>
       </main>
---
+++ components/AppSidebar.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import {
+  Bot,
+  CreditCard,
+  Home,
+  LayoutDashboard,
+  Plus,
+  Presentation,
+} from "lucide-react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meetings",
+    icon: Presentation,
+  },
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  },
+];
+
+const projects = [
+  {
+    name: "Project1",
+  },
+  {
+    name: "Project2",
+  },
+  {
+    name: "project3",
+  },
+];
+
+const AppSidebar = () => {
+  const pathname = usePathname();
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+      <SidebarHeader>Logo</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => {
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className={cn(
+                          {
+                            "!bg-primary !text-white": pathname === item.url,
+                          },
+                          "list-none"
+                        )}
+                      >
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {projects.map((project) => {
+                return (
+                  <SidebarMenuItem key={project.name}>
+                    <SidebarMenuButton asChild>
+                      <div>
+                        <div
+                          className={cn(
+                            "rounded-sm border size-6 flex items-center justify-center text-sm text-primary uppercase",
+                            { "bg-primary text-white": true }
+                          )}
+                        >
+                          {project.name[0]}
+                        </div>
+                        <span>{project.name}</span>
+                      </div>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+
+              <div className="h-2"></div>
+              <SidebarMenuItem>
+                <Link href={"/create"}>
+                  <Button
+                    size={"sm"}
+                    variant={"outline"}
+                    className="w-fit cursor-pointer"
+                  >
+                    <Plus />
+                    Create Project
+                  </Button>
+                </Link>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
